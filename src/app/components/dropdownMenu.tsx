d1b4f2c57a4c11e8e24cd3b98a86d41731f45987
Julio Camargo <jcamargo@hotmail.co.uk>
2023-10-06 12:19:49 +0100

    nearly done

--- src/app/components/dropdownMenu.tsx
+++ src/app/components/dropdownMenu.tsx
@@ -5,33 +5,33 @@ import { useState, useMemo } from "react";
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react"
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
-
+// Main component
 export default function DropdownComponent() {
-  const [selectedKeys, setSelectedKeys] = useState(new Set(["Serif"]));
-
+  // state for selected keys
+  const [selectedKeys, setSelectedKeys] = useState(new Set(["serif"]));
+  // get selected value
   const selectedValue = Array.from(selectedKeys).join(", ").replaceAll("_", " ");
-
-  const handleSelectionChange = (keys: Set<string>) => {
-    setSelectedKeys(keys);
+  // handle selection change
+  const handleSelectionChange = (keys: any) => {
+    // set selected keys
+    setSelectedKeys(new Set(Array.from(Set.prototype.values.call(keys))));
   };
-
+  
   return (
-    <section className="ml-20">
+    <section className="ml-20 dark:text-pink-600">
       <Dropdown>
         <DropdownTrigger>
           <Button variant="bordered" className="capitalize flex gap-4">
-            {selectedValue} <MdOutlineKeyboardArrowDown className="h-10 w-10 text-gray-600 border-r-2 pr-2" />
+            {selectedValue} <MdOutlineKeyboardArrowDown className="h-10 w-10 text-gray-600 border-r-2 pr-2 dark:text-pink-600 dark:border-r-pink-600" />
           </Button>
         </DropdownTrigger>
         <DropdownMenu
-          aria-label="Single selection example"
+          aria-label="Font"
           variant="flat"
           disallowEmptySelection
           selectionMode="single"
           selectedKeys={selectedKeys}
-          onSelectionChange={(keys: Iterable<string>) => handleSelectionChange(new Set(keys))}
-
-        >
+          onSelectionChange={(keys) => handleSelectionChange(keys)}>
           <DropdownItem key="serif">Serif</DropdownItem>
           <DropdownItem key="mono">Mono</DropdownItem>
           <DropdownItem key="sans serif">Sans Serif</DropdownItem>
